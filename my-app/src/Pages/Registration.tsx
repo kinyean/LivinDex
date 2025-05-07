2377bed6def7a1547b5379399a95a59402ba9c0f
King <kinyean2@gmail.com>
2025-05-07 15:03:02 +0800

    Add login button from reg

--- my-app/src/Pages/Registration.tsx
+++ my-app/src/Pages/Registration.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Registration: React.FC = () => {
+
+    const navigate = useNavigate();
    
     return (
     <>
@@ -9,6 +12,7 @@ const Registration: React.FC = () => {
             <input  type="text" name="name" placeholder="Full Name"/>
             <input  type="text" name="password" placeholder="Password"/>
             <input  type="text" name="email" placeholder="Email"/>
+            <button onClick={() => navigate("/Login")}>Go to Login Page</button>
         </form>
     
     </>);
