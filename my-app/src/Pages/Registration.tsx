6ea9cb557d0a646264a3398a7017d2a0857c34f2
King <kinyean2@gmail.com>
2025-05-07 13:50:02 +0800

    Add Registration Input

--- my-app/src/Pages/Registration.tsx
+++ my-app/src/Pages/Registration.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 
-const Registration = () => {
+const Registration: React.FC = () => {
    
-    return <div> Registration</div>;
+    return (
+    <>
+        <div> Registration</div>
+        <form> 
+            <input  type="text" name="name" placeholder="Full Name"/>
+            <input  type="text" name="password" placeholder="Password"/>
+            <input  type="text" name="email" placeholder="Email"/>
+        </form>
+    
+    </>);
 
 }
 
