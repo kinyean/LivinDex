049fb8e05c0582e64e61f864a75a814d8e137711
King <kinyean2@gmail.com>
2025-05-08 14:17:03 +0800

    Update Registratioin firebase

--- my-app/src/Pages/Registration.tsx
+++ my-app/src/Pages/Registration.tsx
@@ -1,18 +1,44 @@
-import React from "react";
+import  React,  {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import { StringLiteral } from "typescript";
+
+const auth = getAuth();
+
+interface User {
+    email: string;
+    password: string;
+}
 
 const Registration: React.FC = () => {
 
     const navigate = useNavigate();
-   
+    const userDets: User = {email: "", password: ""};
+    const [user, setUser] = useState(userDets);
+
+    const handleClick = () => {
+        createUserWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+            // Signed up 
+            console.log("Registration successful")
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+
+        navigate("/login");
+    }
     return (
     <>
         <div> Registration</div>
         <form> 
-            <input  type="text" name="name" placeholder="Full Name"/>
-            <input  type="text" name="password" placeholder="Password"/>
-            <input  type="text" name="email" placeholder="Email"/>
-            <button onClick={() => navigate("/Login")}>Go to Login Page</button>
+            <input  type="text" name="email" placeholder="Email" onChange={(e) => setUser({...user, email: e.target.value})}/>
+            <input  type="text" name="password" placeholder="Password" onChange={(e) => setUser({...user, password: e.target.value})}/>
+            <button onClick={handleClick}>Go to Login Page</button>
         </form>
     
     </>);
