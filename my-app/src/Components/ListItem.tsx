85871103a75690148f7b50689646bf2a861ef471
Guo Keming <93504708+Kemango@users.noreply.github.com>
2025-05-08 15:59:51 +0800

    Add basic search bar

---
+++ my-app/src/Components/ListItem.tsx
@@ -0,0 +1,14 @@
+export interface ListItem {
+    id: number;
+    name: string;
+  }
+  
+  export const data: ListItem[] = [
+    { id: 1, name: "Tech" },
+    { id: 2, name: "Hardware" },
+    { id: 3, name: "Coffee" },
+    { id: 4, name: "Dining" },
+    { id: 5, name: "Coding" },
+    { id: 6, name: "Movie" }
+  ];
+
