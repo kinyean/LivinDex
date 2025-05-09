1a630027fa1fe66e32e163404fffbcd63d8a4a0e
Guo Keming <93504708+Kemango@users.noreply.github.com>
2025-05-09 14:23:55 +0800

    Add NavBar Component

---
+++ my-app/src/Components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Logo from '../Assets/Unknown User.jpg'
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import '../Styles/Navbar.css';
+
+const Navbar: React.FC = () => {
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <nav>
+      
+      <ul className="sidebar" style={{ display: showSidebar ? 'flex' : 'none' }}>
+        <li onClick={() => setShowSidebar(false)}>Close</li>
+        <li onClick={() => navigate("/profile")}>Profile</li>
+        <li >Wallet</li>
+        <li onClick={() => navigate("/login")}>Log Out</li>
+        
+      </ul>
+      <ul className="navbar">
+        <li className="hover-underline" onClick={() => navigate("/")}>LivinDex</li>
+        <li className="hover-underline">About</li>
+        <li className="hover-underline">Notification</li>
+
+        <img className="avatar" 
+             src={Logo} 
+             alt="Logo" 
+             onClick={() => setShowSidebar(!showSidebar)}
+             style={{ cursor: 'pointer' }}/>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
