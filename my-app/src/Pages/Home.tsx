e826f32b0fe3874ee41702c05b575acbe74d155a
Guo Keming <93504708+Kemango@users.noreply.github.com>
2025-05-10 14:33:49 +0800

    filterNav done

--- my-app/src/Pages/Home.tsx
+++ my-app/src/Pages/Home.tsx
@@ -1,14 +1,12 @@
+import FilterNavbar from "../Components/FilterNavbar";
 import List from "../Components/List";
 import Navbar from "../Components/Navbar"; 
 import React, { useState } from 'react';
 import TextField from "@mui/material/TextField";
-import { useNavigate } from "react-router-dom";
 import '../Styles/Home.css';
 
 const Home = () => {
   const [inputText, setInputText] = useState("");
-
-  const navigate = useNavigate();
 
   let inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const lowerCase = e.target.value.toLowerCase();
@@ -29,6 +27,7 @@ const Home = () => {
               label="Search"
             />
           </div>
+          <FilterNavbar />
           <div className = "userProfile">
           </div>
         </div>
