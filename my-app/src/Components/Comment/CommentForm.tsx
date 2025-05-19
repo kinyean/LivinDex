a0642c844a90af60a944dbefc7509067f8aaf251
Guo Keming <93504708+Kemango@users.noreply.github.com>
2025-05-19 17:36:11 +0800

    checkpoint for comment

--- my-app/src/Components/Comment/CommentForm.tsx
+++ my-app/src/Components/Comment/CommentForm.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useState, useEffect } from "react";
 
-const CommentForm: React.FC = () => {
+interface CommentFormProps {
+  handleSubmit: (text: string, parentId: string | null) => void;
+  submitLabel: string;
+  parentId: string | null;
+}
+
+const CommentForm: React.FC<CommentFormProps> = ({handleSubmit, submitLabel, parentId}) => {
+
+  const [text, setText] = useState("");
+
+  const onSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleSubmit(text, parentId);
+  };
 
   return (
-    <div>CommentForm</div>
+    <form onSubmit={onSubmit}>
+    <textarea
+      className="comment-form-textarea"
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+    />
+    <button className="comment-form-button">
+      {submitLabel}
+    </button>
+  </form>
   );
 };
 
