8171d8072a7e1800e70bbd02b83190b6c1cc45b4
King <kinyean2@gmail.com>
2025-05-28 17:24:58 +0800

    fix deployment

--- my-app/Frontend/src/API/BaseAPI.ts
+++ my-app/Frontend/src/API/BaseAPI.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BaseAPI = axios.create({
-	baseURL:  "http://localhost:3001",
+	baseURL: process.env.REACT_APP_API_URL
 });
 
 export const getAuthToken = () => {
